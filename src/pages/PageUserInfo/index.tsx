261b28df4589231314f905c8a10c5e673e24a206
Natali-Nazarenko <91319785+Natali-Nazarenko@users.noreply.github.com>
2022-01-28 13:26:54 +0200

    add file with interfaces

--- src/pages/PageUserInfo/index.tsx
+++ src/pages/PageUserInfo/index.tsx
@@ -1,34 +1,9 @@
 import { useLocation } from 'react-router-dom';
 import { FormattedMessage } from 'react-intl';
+
 import { UserInfo } from '../../components';
 import styles from './PageUserInfo.module.scss';
-
-interface User {
-    gender: string;
-    name: {
-        title: string;
-        first: string;
-        last: string;
-    };
-    dob: {
-        date: string;
-    };
-    location: {
-        postcode: string;
-        state: string;
-        city: string;
-        street: string;
-    };
-    phone: string;
-    picture: {
-        large: string;
-        medium: string;
-        thumbnail: string;
-    };
-    registered: {
-        date: string;
-    };
-}
+import { User } from '../../constans/interfaces';
 
 const PageUserInfo = () => {
     const { state } = useLocation();
